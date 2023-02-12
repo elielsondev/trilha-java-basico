8ace889df54384edc339d35fdb5c31e6d13a3595
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-02-12 16:17:56 -0300

    feat - operadores aritmétcos

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -3,5 +3,26 @@ public class Operadores {
         // Operadores de Atribuição (=):
         String mensagem = "Deus é bom o tempo todo, e o tempo todo Deus é bom";
         System.out.println(mensagem);
+
+        // Operadores aritmétcos (+), (-), (*), (/) e (%):
+        int numero1 = 10;
+        int numero2 = 2;
+
+        int soma = numero1 + numero2;
+
+        int subtracao = numero1 - numero2;
+
+        int multiplicacao = numero1 * numero2;
+
+        int divisao = numero1 / numero2;
+
+        int modulo = numero1 % numero2;
+
+        System.out.println(soma); // 12
+        System.out.println(subtracao); // 8
+        System.out.println(multiplicacao); // 20
+        System.out.println(divisao); // 5 
+        System.out.println(modulo); // 0
+
     }
 }
