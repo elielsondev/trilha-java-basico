68fcd2457ec47444021c833e778c6a7cf3dda32e
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-02-14 21:03:00 -0300

    feat - metodos

---
+++ metodos/src/MinhaClass.java
@@ -0,0 +1,18 @@
+public class MinhaClass {
+    /*
+    Os métodos, correspondem a funções ou sub-rotinas
+    disponíveis dentro de nossas classes.
+    */
+    public static void main(String[] args) throws Exception {
+        // Eu sou um método principal
+        
+        /* 
+         Geralmente esse método é usado apenas na class 
+        principal de cada projeto 
+        */
+    }
+
+    public void MeuMetodo () {
+        // Eu sou um método generico;
+    }
+}
