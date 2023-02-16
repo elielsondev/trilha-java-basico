ceef7bf55ab3c45adb98af315085e119ee83c612
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-02-16 18:28:16 -0300

    feat - adicionando os requisitos para dar inicio ao projeto.

--- metodos/src/SmartTv.java
+++ metodos/src/SmartTv.java
@@ -1,4 +1,19 @@
 
+// Vamos criar um exemplo de uma classe para representar uma SmartTV onde:
 public class SmartTv {
-    
+    // Ela tenha as características: ligada (boolean), canal (int) e volume (int);
+
+    // Nossa TV poderá ligar e desligar e assim mudar o estado ligada;
+
+    // Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1;
+        // O volume deve apenas de 0 a 100;
+
+    // Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente.
+        // Os canais são de 1 a 100
+        
+        /* OBS: Caso a tv esteja no canal 1 e o usuário volte 1 canal,
+        o canal sitonizado será o 100.
+        Porém se a tv estiver no canal 100 e o usuário avançar um canal,
+        o canal sitonizado será o 1.
+         */
 }
